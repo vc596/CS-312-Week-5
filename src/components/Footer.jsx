84d761bf91f52192e89af32cca104027983b7f5c
vc596 <131838737+vc596@users.noreply.github.com>
2024-07-10 18:58:21 -0700

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return <footer>Copyright © {currentYear}</footer>;
+  return;
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is a test note.</h1>
+      <p>This is where content would be.</p>
+    </div>
+  );
+  return;
+}
+
+export default Note;
